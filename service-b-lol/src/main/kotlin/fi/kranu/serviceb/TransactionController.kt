69cadd030dba1f103922ed9df8027dccc067077e
Jarno Tuovinen <spedepekka@gmail.com>
2026-02-13 10:45:18 +0200

    Service B proper functionality
    Wait and better URL internal-transactions

--- service-b-lol/src/main/kotlin/fi/kranu/serviceb/TransactionController.kt
+++ service-b-lol/src/main/kotlin/fi/kranu/serviceb/TransactionController.kt
@@ -1,21 +1,28 @@
 package fi.kranu.serviceb
 
+import org.slf4j.LoggerFactory
 import org.springframework.web.bind.annotation.*
 import java.util.concurrent.CopyOnWriteArrayList
+import kotlin.random.Random
 
 @RestController
-@RequestMapping("/transactions")
+@RequestMapping("/internal-transactions")
 class TransactionController {
-    private val transactions = CopyOnWriteArrayList<Transaction>()
+    private val logger = LoggerFactory.getLogger(TransactionController::class.java)
 
     @PostMapping
-    fun createTransaction(@RequestBody transaction: Transaction): Transaction {
-        transactions.add(transaction)
-        return transaction
-    }
+    fun handleTransaction(@RequestBody transaction: Transaction): Transaction {
+        // Randomize between 500 ms and 3000 ms
+        val delay = Random.nextInt(500,3000)
+        logger.info("Waiting for {} ms", delay)
+        // Simulate transaction processing with random delay
+        try {
+            Thread.sleep((delay).toLong())
+        } catch (e: InterruptedException) {
+            Thread.currentThread().interrupt()
+            throw RuntimeException("Transaction handling interrupted", e)
+        }
 
-    @GetMapping
-    fun getAllTransactions(): List<Transaction> {
-        return transactions
+        return transaction
     }
 }
