c664e13d319ead769199a8b9103a1199bf3da784
Jarno Tuovinen <spedepekka@gmail.com>
2026-02-14 13:07:59 +0200

    Remove tests from A

--- service-a-api/build.gradle.kts
+++ service-a-api/build.gradle.kts
@@ -24,10 +24,6 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-security")
 	implementation("org.springframework.boot:spring-boot-starter-aop")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
-	testImplementation("org.springframework.boot:spring-boot-starter-security-test")
-	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
-	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
 kotlin {
