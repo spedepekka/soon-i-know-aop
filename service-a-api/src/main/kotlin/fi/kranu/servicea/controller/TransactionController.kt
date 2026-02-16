73b4a7da38a0625f13969f7c9442d4307dc612cd
Jarno Tuovinen <spedepekka@gmail.com>
2026-02-16 10:09:11 +0200

    End user has ability to decide if request will be sent to other service or not

--- service-a-api/src/main/kotlin/fi/kranu/servicea/controller/TransactionController.kt
+++ service-a-api/src/main/kotlin/fi/kranu/servicea/controller/TransactionController.kt
@@ -6,6 +6,7 @@ import org.springframework.http.HttpHeaders
 import org.springframework.web.bind.annotation.*
 import org.springframework.web.client.RestTemplate
 import fi.kranu.servicea.model.Transaction
+import org.springframework.http.HttpEntity
 
 @RestController
 @RequestMapping("/transaction")
@@ -18,17 +19,25 @@ class TransactionController(private val restTemplate: RestTemplate) {
         val correlationId = MDC.get("correlationId")
         logger.info("handleUserTransaction called with transaction: $transaction")
 
-        val headers = HttpHeaders()
-        if (correlationId != null) {
-            headers.set("X-Correlation-Id", correlationId)
-        }
+        // Send to other service if the client asks for it
+        // This has nothing to do with real world and it is just for testing purposes
+        if (transaction.forward) {
+            val headers = HttpHeaders()
+            if (correlationId != null) {
+                headers.set("X-Correlation-Id", correlationId)
+            }
 
-//        val request = HttpEntity(transaction, headers)
-//        try {
-//            restTemplate.postForObject("http://service-b-lol:8080/internal-transaction", request, Transaction::class.java)
-//        } catch (e: Exception) {
-//            logger.error("Failed to call service-b", e)
-//        }
+            val request = HttpEntity(transaction, headers)
+            try {
+                restTemplate.postForObject(
+                    "http://service-b-lol:8080/internal-transaction",
+                    request,
+                    Transaction::class.java
+                )
+            } catch (e: Exception) {
+                logger.error("Failed to call service-b", e)
+            }
+        }
 
         return transaction
     }
--- service-a-api/src/main/kotlin/fi/kranu/servicea/model/Transaction.kt
+++ service-a-api/src/main/kotlin/fi/kranu/servicea/model/Transaction.kt
@@ -3,5 +3,7 @@ package fi.kranu.servicea.model
 data class Transaction(
     val fromAccount: String,
     val toAccount: String,
-    val value: Float
+    val value: Float,
+    // Just a dirty switch to send the request to other service or not
+    val forward: Boolean = false
 )
