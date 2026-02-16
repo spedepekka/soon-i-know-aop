e2135be41fc4a95108557639b214c8f6dc08ae17
Jarno Tuovinen <spedepekka@gmail.com>
2026-02-16 10:15:53 +0200

    Fix IntelliJ problems with aspects

--- service-a-api/build.gradle.kts
+++ service-a-api/build.gradle.kts
@@ -23,6 +23,7 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("org.springframework.boot:spring-boot-starter-security")
 	implementation("org.springframework.boot:spring-boot-starter-aop")
+	implementation("org.springframework:spring-aspects")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 }
 
